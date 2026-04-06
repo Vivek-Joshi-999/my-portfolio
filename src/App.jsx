3601b015176ef6d4640294bb2cc40de2197502b9
Vivek Joshi <your-email@example.com>
2026-04-06 22:48:10 +0530

    Initial commit: Vite React project my-portfolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+      <h1 className='text-red-500'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
