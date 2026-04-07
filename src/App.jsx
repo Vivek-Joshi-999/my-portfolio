03f3c4af227fc5385d62231834e27003cdd9da71
Vivek Joshi <your-email@example.com>
2026-04-08 01:14:06 +0530

    navbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className='bg-blue-400 text-white flex  flex-col items-center justify-center'>
+<Navbar></Navbar>
 
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
-  <h1>HELLO</h1>
     </div>
   )
 }
