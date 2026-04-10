8a7d7b3cd5e2a0083cedf8976e5a4421066ddc3e
Vivek Joshi <your-email@example.com>
2026-04-11 00:38:57 +0530

    [Navbar]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,9 +28,15 @@ const Navbar = () => {
   initial={{ y: -120, opacity: 0 }}
   animate={{ y: 0, opacity: 1 }}
   transition={{ duration: 0.5 }}
-  className='z-50 relative p-4'
+  className='fixed top-0 left-0 right-0 z-50 transition-all duration-300'
 >
-  <h1 className='text-white text-xl'>Hello</h1>
+ 
+<div className='max-w-7xl border border-white'>
+  <h1 className="text-white text-lg sm:text-xl font-bold">
+         Port<span className="text-indigo-400">Folio</span>
+       </h1>
+</div>
+
 </motion.header>
   )
 }
