1f748f2d71372b926e59d962a447547742b6e941
Vivek Joshi <your-email@example.com>
2026-04-11 11:32:41 +0530

    new navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,16 +28,16 @@ const Navbar = () => {
   initial={{ y: -120, opacity: 0 }}
   animate={{ y: 0, opacity: 1 }}
   transition={{ duration: 0.5 }}
-  className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 flex justify-between items-center
+  className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 
   ${
-    scrolled ? "bg-slate-950/70  backdrop-blur-md boderborder-slate-800/50 shadow-lg shadow-black/10 py-5":
-    "fixed top-0 left-0 right-0 z-50 transition-all duration-300 border-b border-transparent bg-transparent py-5"
+    scrolled ? "fixed top-0 left-0 right-0 z-50 transition-all duration-300 border-b border-transparent bg-slate-950/70 backdrop-blur-md border-slate-800/50 py-2 shadow-lg shadow-black/10 ":
+    "fixed top-0 left-0 right-0 z-50 transition-all duration-300 border-b border-transparent bg-transparent py-2"
   }
   
   `}
 >
  
-<div className='max-w-7xl border border-white '>
+<div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-10 py-3 flex  '>
   <h1 className="text-white text-lg sm:text-xl font-bold">
          Port<span className="text-indigo-400">Folio</span>
        </h1>
