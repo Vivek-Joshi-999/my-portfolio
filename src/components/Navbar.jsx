893373a35ea39cfa410331aefe79aae5184e2327
Vivek Joshi <your-email@example.com>
2026-04-11 11:19:34 +0530

    css change in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,10 +28,16 @@ const Navbar = () => {
   initial={{ y: -120, opacity: 0 }}
   animate={{ y: 0, opacity: 1 }}
   transition={{ duration: 0.5 }}
-  className='fixed top-0 left-0 right-0 z-50 transition-all duration-300'
+  className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 flex justify-between items-center
+  ${
+    scrolled ? "bg-slate-950/70  backdrop-blur-md boderborder-slate-800/50 shadow-lg shadow-black/10 py-5":
+    "fixed top-0 left-0 right-0 z-50 transition-all duration-300 border-b border-transparent bg-transparent py-5"
+  }
+  
+  `}
 >
  
-<div className='max-w-7xl border border-white'>
+<div className='max-w-7xl border border-white '>
   <h1 className="text-white text-lg sm:text-xl font-bold">
          Port<span className="text-indigo-400">Folio</span>
        </h1>
